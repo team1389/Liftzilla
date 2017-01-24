4d435fd0c22e7fc193f943301cd62c5e27b6bd34
benlo <benlo@LAPTOP-AN0Q8HA4>
2017-01-24 17:52:06 -0500

    its all good

--- Liftzilla/src/org/usfirst/frc/team1389/robot/RobotHardware.java
+++ Liftzilla/src/org/usfirst/frc/team1389/robot/RobotHardware.java
@@ -38,7 +38,7 @@ public class RobotHardware extends RobotLayout {
 	}
 
 	private void initDriveTrain() {
-		leftA = new CANTalonHardware(false, t, new CAN(can_LEFT_MOTOR_A), registry);
+		leftA = new CANTalonHardware(false, false, new CAN(can_LEFT_MOTOR_A), registry);
 		rightA = new CANTalonHardware(true, false, new CAN(can_RIGHT_MOTOR_A), registry);
 		leftB = new CANTalonHardware(false, false, new CAN(can_LEFT_MOTOR_B), registry);
 		rightB = new CANTalonHardware(true, false, new CAN(can_RIGHT_MOTOR_B), registry);
